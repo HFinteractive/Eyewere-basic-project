2336dfe114e473f723da76512bb37730fd287779
happydiego <diego.bonilla@happyfinish.com>
2015-05-15 12:38:27 +0100

    Working project

---
+++ Assets/HappyFinish/Scripts/SetResolution.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SetResolution : MonoBehaviour 
+{
+
+	public int ScreenWidth = 2560;
+	public int ScreenHeight = 1440;
+
+	// Use this for initialization
+	void Start () 
+	{
+		ChangeScale ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if ( transform.localScale.x != ScreenWidth || transform.localScale.z != ScreenHeight )
+		{
+			ChangeScale();
+		}
+	}
+
+	void ChangeScale()
+	{
+		transform.localScale = new Vector3 (ScreenWidth, 1, ScreenHeight);
+	}
+}
---
+++ README.md
@@ -0,0 +1,12 @@
+# Eyewear Basic Project
+
+	# Oculus SDK 0.5 integration
+	# Vuforia SDK 4.2.3 Digital Eyewear Beta integration
+	
+	# Working project
+	# HappyFinish/Scene/default.scene
+	
+	# Don't forget to add the sign for your mobile
+	# https://developer.oculus.com/osig/
+	
+	
